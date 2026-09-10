c17f2785d9c15725383acc24d60f917d70754241
Parmindar Singh <parmindarmundra@gmail.com>
2026-09-09 20:32:31 -0400

    updating necessary files

--- apputil.py
+++ apputil.py
@@ -1,3 +1,35 @@
 
 
 # add code below ...
+
+def palindrome(word):
+    word = word.lower()
+    word = word.replace(" ", "")
+    word = word.replace(",", "")
+    word = word.replace(".", "") 
+    
+    return word == word[::-1] 
+
+print(palindrome("racecar"))
+print(palindrome("Nurses Run"))
+print(palindrome("Sit on a potato pan, Otis."))
+
+def parentheses(sequence):
+    count = 0
+    
+    for char in sequence:
+        if char == "(":
+            count += 1
+        elif char == ")":
+            count -= 1
+        
+        if count < 0:
+            return False
+    
+    return count == 0
+
+print(parentheses("((blah)()()())"))
+print(parentheses("(((())blee))"))
+print(parentheses("(()hello((())()))"))
+print(parentheses("((((((())"))
+print(parentheses("()))"))
